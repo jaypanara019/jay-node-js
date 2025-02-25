88a57ed5b4e0250aa6bd5e5a3d52ea62812d9ccc
jaypanara019 <160013245+jaypanara019@users.noreply.github.com>
2025-02-25 09:24:50 +0530

    2-25-25(task-calculator)

---
+++ Calculator/calculator.js
@@ -0,0 +1,6 @@
+module.exports = {
+    addition: (a, b) => a + b,
+    subtraction: (a, b) => a - b,
+    multiplication: (a, b) => a * b,
+    divison: (a, b) => (b !== 0 ? a / b : "can not divide by zero.")
+};
