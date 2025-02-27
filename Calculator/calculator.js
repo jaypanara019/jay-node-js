65d9d3085ab2273c60a1b2d3ec9ba156642af36d
jaypanara019 <160013245+jaypanara019@users.noreply.github.com>
2025-02-27 09:42:14 +0530

    Delete Calculator directory

--- Calculator/calculator.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    addition: (a, b) => a + b,
-    subtraction: (a, b) => a - b,
-    multiplication: (a, b) => a * b,
-    divison: (a, b) => (b !== 0 ? a / b : "can not divide by zero.")
-};
